2f050ee6b065b24360d8ac649e11d69fe925c0d2
linux_china <libing.chen@gmail.com>
2022-10-27 14:04:36 +0800

    test: protobuf test

---
+++ src/test/java/org/mvnsearch/ProtobufTest.java
@@ -0,0 +1,22 @@
+package org.mvnsearch;
+
+import com.google.protobuf.FieldMask;
+import com.google.protobuf.util.FieldMaskUtil;
+import org.junit.jupiter.api.Test;
+import org.mvnsearch.service.AccountResponse;
+import org.mvnsearch.service.GetAccountRequest;
+
+
+public class ProtobufTest {
+
+    @Test
+    public void testFieldMask() {
+        FieldMask fieldMask = FieldMask.newBuilder().addPaths("nick").build();  // FieldMaskUtil.fromFieldNumbers(AccountResponse.class, AccountResponse.NICK_FIELD_NUMBER);
+        final AccountResponse accountDTO = AccountResponse.newBuilder().setId(1).setNick("nick").build();
+        AccountResponse.Builder accountWithMaskedFields = AccountResponse.newBuilder();
+        FieldMaskUtil.merge(fieldMask, accountDTO, accountWithMaskedFields);
+        final AccountResponse accountResponse = accountWithMaskedFields.build();
+        System.out.println("id: " + accountResponse.getId());
+        System.out.println("nick: " + accountResponse.getNick());
+    }
+}
