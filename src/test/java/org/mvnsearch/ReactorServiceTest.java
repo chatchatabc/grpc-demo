8cc810e457aff2f2b21b09b8be1aede2849f5e54
linux_china <libing.chen@gmail.com>
2022-10-27 14:05:24 +0800

    test: gRPC service test

---
+++ src/test/java/org/mvnsearch/ReactorServiceTest.java
@@ -0,0 +1,49 @@
+package org.mvnsearch;
+
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.mvnsearch.service.GetAccountRequest;
+import org.mvnsearch.service.ReactorAccountServiceGrpc;
+import reactor.core.publisher.Mono;
+
+/**
+ * reactor service test
+ *
+ * @author linux_china
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class ReactorServiceTest {
+    private ManagedChannel channel;
+    private ReactorAccountServiceGrpc.ReactorAccountServiceStub stub;
+
+    @BeforeAll
+    public void setUp() {
+        channel = ManagedChannelBuilder.forAddress("localhost", 50052).usePlaintext().build();
+        stub = ReactorAccountServiceGrpc.newReactorStub(channel);
+    }
+
+    @AfterAll
+    public void tearDown() {
+        channel.shutdownNow();
+    }
+
+    /**
+     * test for say hello
+     */
+    @Test
+    public void testSayHello() throws Exception {
+        Mono<GetAccountRequest> request = Mono.just(GetAccountRequest.newBuilder().setId(1).build());
+        request.as(stub::findAccount)
+                .subscribe(response -> {
+                    //logic here
+                    System.out.println(response.getNick());
+                });
+
+        Thread.sleep(1000);
+    }
+}
